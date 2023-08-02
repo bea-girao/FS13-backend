fa081b27ad712156b19f722ebe0dc45f96ef0c7a
JuniorWilder <anabeatrizgirao@gmail.com>
2023-08-01 21:46:11 -0300

    tratamento de bad request

--- aula03/src/Curso/routes.js
+++ aula03/src/Curso/routes.js
@@ -11,26 +11,54 @@ app.get('/cursos', async(req, res) => {
 app.get('/cursos/:id', async (req, res) => {
     let dados = await database.executar(`SELECT * FROM tb_curso WHERE id=${req.params.id}`);
 
+    if (dados.length === 0) {
+        res.status(404);
+        res.send();
+    }
+
     res.send(dados[0]);
 });
 
 app.post('/cursos', async(req, res) => {
+    if (!req.body.nome) {
+        //BAD REQUEST
+        res.status(400).json({
+            "erro": "Nome é obrigatório"
+        });
+    }
+
     let {nome, carga_horaria} = req.body;
 
-    await database.executar(
+    let dados = await database.executar(
         `INSERT INTO tb_curso (nome, carga_horaria) VALUES ('${nome}', '${carga_horaria}')`
     );
+
+    req.body.id = dados.insertId; //mysql2 -> insere o campo Id no corpo da requisicao
+
     res.status(201); //CREATED
     res.send(req.body);
 });
 
-app.patch('/cursos/:id', (req, res) => {
-    res.end('vai editar um curso existente');
+app.patch('/cursos/:id', async(req, res) => {
+    let {nome, carga_horaria} = req.body;
+    let id = req.params.id;
+
+    await database.executar(
+        `UPDATE tb_curso SET nome='${nome}', carga_horaria='${carga_horaria}' WHERE id='${id}')`
+    );
+
+    req.body.id = dados.id;
+    res.send(req.body);
 });
 
 app.delete('/cursos/:id', async(req, res) => {
-    await database.executar(`DELETE FROM tb_curso WHERE id=${req.params.id}`);
+    let dados = await database.executar(`DELETE FROM tb_curso WHERE id=${req.params.id}`);
     
+    if (dados.affectedRows === 0) {
+        res.status(404);
+        res.end();
+    }
+
     res.status(204); //SUCESSO COM STATUS 'NO CONTENT': nao retorna dados
     res.end();
 });
